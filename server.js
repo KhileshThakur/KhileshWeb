9435148bd8678b453c440d0de0bf9c9188c6ca2f
Khilesh Thakur <omthakur6640@gmail.com>
2025-12-03 00:41:56 +0530

    production issue fixed

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const frontendPath = path.join(__dirname, "frontend", "dist");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
